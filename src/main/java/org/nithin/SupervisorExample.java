c5d7d8380c5fd7945dc0308b81252f80799cf5da
Nithin Titus <nithin22796@gmail.com>
2024-08-16 20:02:04 -0500

    Multi-threading basics

---
+++ src/main/java/org/nithin/SupervisorExample.java
@@ -0,0 +1,42 @@
+package org.nithin;
+
+public class SupervisorExample {
+
+    public static void main(String[] args) {
+        Worker1 w1 = new Worker1();
+        Worker2 w2 = new Worker2();
+
+        try {
+            w1.executeWork();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        w2.executeWork();
+    }
+}
+
+class Worker1 {
+    public void executeWork() throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(100);
+            System.out.println("Worker 1 is executing : " + i);
+        }
+    }
+}
+
+class Worker2 {
+
+    public void executeWork(){
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Worker 2 is executing: " + i);
+        }
+    }
+
+}
---
+++ src/main/java/org/nithin/SupervisorExampleWithThread.java
@@ -0,0 +1,40 @@
+package org.nithin;
+
+public class SupervisorExampleWithThread {
+
+    public static void main(String[] args) {
+        ParallelWorker1 p1 = new ParallelWorker1();
+        ParallelWorker2 p2 = new ParallelWorker2();
+
+        p1.start();
+        p2.start();
+    }
+}
+
+class ParallelWorker1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Worker 1 is executing : " + i);
+        }
+    }
+}
+
+class ParallelWorker2 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Worker 2 is executing : " + i);
+        }
+    }
+}
